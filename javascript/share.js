bb02f377b1e2971256e943f224e1c1f0a1d85dd9
Aman Kumar <106679817+Aman2104@users.noreply.github.com>
2022-08-22 19:05:06 +0530

    Update share.js

--- javascript/share.js
+++ javascript/share.js
@@ -1,6 +1,7 @@
 let sharebtn = document.querySelector(".fa-share-alt");
 let title = window.document.title;
 let url1 = window.document.href;
+url1 = url1.replace("undefined", "");
 sharebtn.addEventListener("click", () => {
     if (navigator.share) {
         navigator.share({
