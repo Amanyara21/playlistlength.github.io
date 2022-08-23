fecebac1ca37aff7ca48111301f912732cc92f76
Aman2104 <uietcseamankumar2125@kuk.ac.in>
2022-08-23 08:39:05 +0530

    Complete

--- javascript/share.js
+++ javascript/share.js
@@ -21,15 +21,17 @@ let btn = document.getElementById("menu")
 let arr = document.getElementsByClassName("nav-linker")
 let head = document.getElementsByClassName("heading")[0];
 let ul_show = document.getElementsByClassName("linking")[0];
-
+let i =0;
 btn.addEventListener("click", ()=>{
     head.classList.toggle("expand")
     btn.classList.toggle("fa-bars")
     btn.classList.toggle("fa-close")
     ul_show.classList.toggle("show")
-    ul_show.classList.toggle("hide")
+    i++;
+    if(i>1){
+        ul_show.classList.toggle("hide")
+    }
     for (let i = 0; i < arr.length; i++) {
         arr[i].classList.toggle("act")
     }
 })
-
